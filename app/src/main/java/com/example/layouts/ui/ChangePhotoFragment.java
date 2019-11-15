891f9a5eb27c8df396a8f46ca5010028a182b98d
Veniamin Borovikov <vnbor08@mail.ru>
2019-11-15 11:42:01 +0400

    Complete ChangePhotoFragment

---
+++ app/src/main/java/com/example/layouts/ui/ChangePhotoFragment.java
@@ -0,0 +1,116 @@
+package com.example.layouts.ui;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
+import androidx.fragment.app.DialogFragment;
+
+import com.example.layouts.R;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.app.Activity.RESULT_OK;
+
+public class ChangePhotoFragment extends DialogFragment {
+
+    private static final int REQUEST_TAKE_PHOTO = 1;
+
+    private File photoFile;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.profile_change_photo_dialog, container, false);
+
+        TextView cameraTextView = view.findViewById(R.id.change_photo_dialog_camera_text_view);
+        cameraTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePictureIntent();
+
+            }
+        });
+
+        TextView deletePhotoTextView = view.findViewById(R.id.change_photo_dialog_delete_text_view);
+        deletePhotoTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(getActivity() != null){
+                    ImageView imageView = getActivity().findViewById(R.id.profile_photo_image_view);
+                    Picasso.with(getContext()).load(R.drawable.user_icon).into(imageView);
+                    dismiss();
+                }
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            if(getActivity() != null) {
+                ImageView imageView = getActivity().findViewById(R.id.profile_photo_image_view);
+                Picasso.with(getContext()).load(photoFile).fit().centerCrop().into(imageView);
+                dismiss();
+            }
+        }
+    }
+
+    private void takePictureIntent() {
+        Intent pictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if(getActivity() != null) {
+            if (pictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+                photoFile = null;
+                try {
+                    photoFile = createImageFile();
+                } catch (IOException ex) {
+                    Toast.makeText(getActivity(), ex.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+                if (photoFile != null) {
+                    Uri photoURI = FileProvider.getUriForFile(getActivity(),
+                            "com.example.layouts.fileprovider",
+                            photoFile);
+                    pictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                    startActivityForResult(pictureIntent, REQUEST_TAKE_PHOTO);
+                }
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File image = null;
+        if(getActivity() != null) {
+            File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+             image = File.createTempFile(
+                    imageFileName,
+                    ".jpg",
+                    storageDir
+            );
+        }
+        return image;
+    }
+
+
+}
