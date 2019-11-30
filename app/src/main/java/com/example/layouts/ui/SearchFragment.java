266638a98755a063e8bd0406ba9dbc19377ad62d
Veniamin Borovikov <vnbor08@mail.ru>
2019-11-30 13:46:29 +0400

    Done

--- app/src/main/java/com/example/layouts/ui/SearchFragment.java
+++ app/src/main/java/com/example/layouts/ui/SearchFragment.java
@@ -9,7 +9,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -40,9 +39,6 @@ public class SearchFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
-        androidx.appcompat.widget.Toolbar toolbar = view.findViewById(R.id.fragment_search_toolbar);
-        toolbar.inflateMenu(R.menu.search_toolbar_menu);
-
         SearchViewPagerAdapter adapter = new SearchViewPagerAdapter(getChildFragmentManager());
         ViewPager viewPager = view.findViewById(R.id.fragment_search_view_pager);
         viewPager.setAdapter(adapter);
@@ -72,43 +68,9 @@ public class SearchFragment extends Fragment {
                 }
             };
             mSearchView.setOnQueryTextListener(mQueryTextListener);
-            mSearchView.setIconifiedByDefault(false);
-            mSearchView.setQueryHint(getResources().getString(R.string.fragment_search_toolbar_search_view_hint));
-            mSearchView.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
-                @Override
-                public void onFocusChange(View view, boolean b) {
-                    int id = mSearchView.getContext().getResources()
-                            .getIdentifier("android:id/search_view_text_view", null, null);
-                    TextView textView = mSearchView.findViewById(id);
-
-                    if (b) {
-                        mSearchView.setBackgroundColor(getResources().getColor(R.color.white));
-                        textView.setTextColor(getResources().getColor(R.color.cool_grey));
-
-                    } else {
-                        mSearchView.setBackgroundColor(getResources().getColor(R.color.white));
-                        textView.setTextColor(getResources().getColor(R.color.black_87));
-                    }
-                }
-            });
         }
 
         super.onCreateOptionsMenu(menu, inflater);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_search:
-
-                return true;
-
-            default:
-                break;
-        }
-
-        mSearchView.setOnQueryTextListener(mQueryTextListener);
-        return super.onOptionsItemSelected(item);
     }
 
     private class SearchViewPagerAdapter extends FragmentPagerAdapter {
